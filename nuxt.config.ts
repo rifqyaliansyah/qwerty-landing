ff1329823f071f91e110acacb90925a3219b3cdb
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2025-12-08 19:22:14 +0700

    chore: update nuxt.config.ts and package files to include @pinia/nuxt and @types/node

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  
+  modules: ['@pinia/nuxt'],
+
+  runtimeConfig: {
+    // Private keys (server-side only)
+    // apiSecret: '',
+
+    // Public keys (exposed to client)
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL
+    }
+  },
   devServer: {
     host: '0.0.0.0',
     port: 3000,
   },
   app: {
     head: {
+      title: 'My App',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
       link: [
         // Download early (non-blocking)
         { rel: "preload", as: "style", href: "/css/plugins.css" },
